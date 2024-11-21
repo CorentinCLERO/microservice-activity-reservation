35eac8c0e9d188e1d9ce381ed3e4cf12a5d19616
David Chau <davidc2509@gmail.com>
2024-11-21 22:56:11 +0100

    Added fix token validation problem

--- api-gateway/src/config/config.js
+++ api-gateway/src/config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   port: process.env.PORT || 3000,
-  jwtSecret: process.env.JWT_SECRET || 'your_secret_here',
+  jwtSecret: process.env.JWT_SECRET || 'microservice',
   services: {
     users: process.env.USER_SERVICE_URL || 'http://user-service:3001',
     activities: process.env.ACTIVITY_SERVICE_URL || 'http://activity-service:3002',
--- api-gateway/src/middleware/auth.js
+++ api-gateway/src/middleware/auth.js
@@ -1,16 +1,19 @@
 const jwt = require('jsonwebtoken');
 const config = require('../config/config');
+const fs = require('fs')
 
 const authenticateToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
-
+    const publicKey = fs.readFileSync('/usr/src/app/src/middleware/public.pub', { encoding: 'utf-8', flag: 'r' })
+    console.log(publicKey)
     if (!token) {
         return res.status(401).json({ message: 'Token manquant' });
     }
 
-    jwt.verify(token, config.jwt.secret, (err, user) => {
+    jwt.verify(token, publicKey,{ algorithms: ['RS256'] }, (err, user) => {
         if (err) {
+            console.log(err);
             return res.status(403).json({ message: 'Token invalide' });
         }
         req.user = user;
