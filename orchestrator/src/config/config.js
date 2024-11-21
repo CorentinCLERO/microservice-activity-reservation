ff836224df05f8f917b52473354aeea9f4175732
CorentinCLERO <corentin.clero@epsi.fr>
2024-11-21 13:35:07 +0100

    🐛 FIX: env variables

--- orchestrator/src/config/config.js
+++ orchestrator/src/config/config.js
@@ -1,19 +1,19 @@
 require('dotenv').config();
 
 const config = {
-    port: process.env.PORT || 3002,
+    port: process.env.PORT || 3004,
     services: {
         users: {
-            url: process.env.USERS_SERVICE_URL || 'http://users-service:3003'
+            url: process.env.USERS_SERVICE_URL || 'http://users-service:3001'
         },
         activities: {
-            url: process.env.ACTIVITIES_SERVICE_URL || 'http://activities-service:3004'
+            url: process.env.ACTIVITIES_SERVICE_URL || 'http://activities-service:3002'
         },
         notifications: {
             url: process.env.NOTIFICATION_SERVICE_URL || 'http://notification-service:3005'
         },
         reservations: {
-            url: process.env.RESERVATIONS_SERVICE_URL || 'http://reservations-service:3006'
+            url: process.env.RESERVATIONS_SERVICE_URL || 'http://reservations-service:3003'
         }
     },
     jwt: {
